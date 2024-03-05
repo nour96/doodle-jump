bdd0f73e6c7babfefb146b2c431c281f6f27f0fb
Nour Hamdan <nour.ham96@gmail.com>
2024-03-05 18:36:54 +0300

    adding some features

--- app.js
+++ app.js
@@ -1,17 +1,78 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('.grid')
-    const doodler = document.createElement('div')
-    let isGameOver = false;
-    let doodlerLefttSpace = 50;
-    let doodlerBottomSpace = 150;
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const doodler = document.createElement("div");
+  let isGameOver = false;
+  let platformCount = 5;
+  let score = 0
+  let platforms = [];
+  let doodlerLefttSpace = 50;
+  let doodlerBottomSpace = 150;
+  let isJumping = true;
+  let isGoingLeft = false
+  let isGoingRight = false
+  let leftTimerId
+  let rightTimerId
+  let upTimerId
+  let downTimerId
 
+  class Platform {
+    constructor(newPlatBottom) {
+      this.left = Math.random() * 315;
+      this.bottom = newPlatBottom;
+      this.visual = document.createElement("div");
 
-    
-    function createDoodler() {
-        grid.appendChild(doodler)
-        doodler.classList.add('doodler')
-        doodler.style.bottom = doodlerBottomSpace + 'px'
+      const visual = this.visual;
+      visual.classList.add("platform");
+      visual.style.left = this.left + "px";
+      visual.style.bottom = this.bottom + "px";
+      grid.appendChild(visual);
     }
+  }
 
-    createDoodler()
-})
+  function createPlatforms() {
+    for (let i = 0; i < platformCount; i++) {
+      let platGap = 600 / platformCount;
+      let newPlatBottom = 100 + i * platGap;
+      let newPlatform = new Platform(newPlatBottom);
+      platforms.push(newPlatform);
+    }
+  }
+
+  function movePlatforms() {
+    if (doodlerBottomSpace > 200) {
+      platforms.forEach((platform) => {
+        platform.bottom -= 4;
+        let visual = platform.visual;
+        visual.style.bottom = platform.bottom + "px";
+
+        if (platform.bottom < 10) {
+          let firstPlatform = platforms[0].visual;
+          firstPlatform.classList.remove("platform");
+          platforms.shift();
+          console.log(platforms);
+          score++;
+          let newPlatform = new Platform(600)
+          platforms.push(newPlatform)
+        }
+      });
+    }
+  }
+
+  function createDoodler() {
+    grid.appendChild(doodler);
+    doodler.classList.add("doodler");
+    doodlerLefttSpace = platforms[0].left;
+    doodler.style.left = doodlerLefttSpace + "px";
+    doodler.style.bottom = doodlerBottomSpace + "px";
+  }
+
+  function start() {
+    if (!isGameOver) {
+      createPlatforms();
+      createDoodler();
+      setInterval(movePlatforms,30);
+    }
+  }
+
+  start();
+});
